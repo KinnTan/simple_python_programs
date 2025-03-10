83b15ed039f7558061e0a85ff2ae1c6b6b571e47
Kinn Tan <tankinnr@gmail.com>
2025-03-11 00:10:08 +0800

    Create product_two_numbers.py to get product of two numbers

---
+++ Batch 1/product_two_numbers.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+num1 = int(input("Input First Number: "))
+num2 = int(input("Input Second Number: "))
+
+product = num1 * num2
+
+print("The sum of", num1, "and", num2, "is:", product)
