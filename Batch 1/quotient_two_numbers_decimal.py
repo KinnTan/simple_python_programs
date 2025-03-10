2cda76307350bdb5f2846e39bc998d22e9280a2f
Kinn Tan <tankinnr@gmail.com>
2025-03-11 00:15:25 +0800

    Create quotient_two_numbers_decimal.py to get quotient of two numbers

---
+++ Batch 1/quotient_two_numbers_decimal.py
@@ -0,0 +1,8 @@
+# Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+num1 = int(input("Input First Number: "))
+num2 = int(input("Input Second Number: "))
+
+quotient = num1/num2
+
+print("The quotient of", num1, "and", num2, "is:", quotient)
