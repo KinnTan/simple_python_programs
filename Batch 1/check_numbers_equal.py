7fbd770a31fec627cb1db99bbc5b027f04e8765b
Kinn Tan <tankinnr@gmail.com>
2025-03-11 00:03:38 +0800

    Create check_numbers_equal.py to check if two numbers are equal

---
+++ Batch 1/check_numbers_equal.py
@@ -0,0 +1,9 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+num1 = int(input("Input First Number: "))
+num2 = int(input("Input Second Number: "))
+
+if num1 == num2:
+    print("Equal")
+else:
+    print("The numbers are not equal.")
