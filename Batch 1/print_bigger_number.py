198ca7f65d1987fcb5557b20102cc623bd8451ea
Kinn Tan <tankinnr@gmail.com>
2025-03-10 23:55:18 +0800

    Add program to compare and print the bigger number in print_bigger_number.py

---
+++ Batch 1/print_bigger_number.py
@@ -0,0 +1,9 @@
+#Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
+
+num1 = int(input("Input First Number: "))
+num2 = int(input("Input Second Number: "))
+
+if num1 > num2:
+    print (num1)
+else:
+    print (num2)
