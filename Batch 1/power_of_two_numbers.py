58a3e0d76cac1a019f0ff8c15dcf3f8c5eb23817
Kinn Tan <tankinnr@gmail.com>
2025-03-11 00:21:52 +0800

    Create power_of_two_numbers.py to calculate power of a number

---
+++ Batch 1/power_of_two_numbers.py
@@ -0,0 +1,8 @@
+# Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+num1 = int(input("Input First Number: "))
+num2 = int(input("Input Second Number: "))
+
+power = num1**num2
+
+print(num1, "raised to", num2, "is:", power)
