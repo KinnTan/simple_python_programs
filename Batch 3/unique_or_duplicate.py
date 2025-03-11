6ecf9fed0b09d5fcb12dfb7fdc8db269533c0a17
Kinn Tan <tankinnr@gmail.com>
2025-03-11 21:26:24 +0800

    -initialize number_list -print Unique and append inputted number if not in number_list

--- Batch 3/unique_or_duplicate.py
+++ Batch 3/unique_or_duplicate.py
@@ -1,6 +1,10 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
-
+number_list = []
 while True:
     numbers = int(input())
+    if numbers not in number_list:
+        number_list.append(numbers)
+        print("Unique")
+
 
