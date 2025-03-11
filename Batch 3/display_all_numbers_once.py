f2bb350821b4b1d30c57a308cbb330d806da1b48
Kinn Tan <tankinnr@gmail.com>
2025-03-11 21:00:54 +0800

    - initialize display_list - add input label for clarity - check if number already in display_list before append

--- Batch 3/display_all_numbers_once.py
+++ Batch 3/display_all_numbers_once.py
@@ -1,4 +1,9 @@
 #Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
+display_list = []
 for i in range(1,11):
-    numbers = int(input())
+    numbers = int(input("Enter a number: "))
+    if numbers not in display_list:
+        display_list.append(numbers)
+
+print(display_list)
