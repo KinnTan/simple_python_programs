00f1c707bc6be1161b310145c859a60d84f6d42b
Kinn Tan <tankinnr@gmail.com>
2025-03-11 09:17:55 +0800

    Create difference_two_numbers.py to get difference between two numbers

---
+++ Batch 2/difference_two_numbers.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second Number: "))
+
+difference = num1 - num2
+print ("The difference between", num1, "and", num2, "is:", difference)
