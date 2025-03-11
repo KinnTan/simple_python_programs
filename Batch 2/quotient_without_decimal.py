ea662860794ea0bb224786fe9d766c48de1f45bd
Kinn Tan <tankinnr@gmail.com>
2025-03-11 09:27:30 +0800

    Create quotient_without_decimal.py to get quotient without decimals

---
+++ Batch 2/quotient_without_decimal.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second Number: "))
+
+quotient = num1/num2
+print ("The quotient without the decimal point is:", int(quotient))
