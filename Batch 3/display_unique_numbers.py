60707aacf501105c49b8f3cf2a7a287457c230a0
Kinn Tan <tankinnr@gmail.com>
2025-03-11 20:51:06 +0800

    add input and print labels for clarity

--- Batch 3/display_unique_numbers.py
+++ Batch 3/display_unique_numbers.py
@@ -4,12 +4,11 @@ numbers_list = []
 unique_numbers_list = []
 
 for i in range(1,11):
-    numbers = int(input())
+    numbers = int(input("Enter a number:"))
     numbers_list.append(numbers)
 
 for numbers in numbers_list:
     if numbers_list.count(numbers) == 1:
         unique_numbers_list.append(numbers)
 
-
-print(unique_numbers_list)
+print("Numbers without duplicates:", unique_numbers_list)
