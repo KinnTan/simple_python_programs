2579051144eadf066edba4c2d832fd80462e7329
Kinn Tan <tankinnr@gmail.com>
2025-03-12 21:48:37 +0800

    Create most_frequent_number.py - added while loop to take user input continuously

---
+++ Batch 4/most_frequent_number.py
@@ -0,0 +1,6 @@
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most numbe
+
+while True:
+        number = int(input("Enter numbers: "))
+
+
