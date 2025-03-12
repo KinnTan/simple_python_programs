40aa9caf45cb8fbe2b4d0b0a5d61d3d8db7172c1
Kinn Tan <tankinnr@gmail.com>
2025-03-12 22:11:22 +0800

    add identification of the most frequently entered number using max() and print

--- Batch 4/most_frequent_number.py
+++ Batch 4/most_frequent_number.py
@@ -1,4 +1,4 @@
-#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most numbe
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 number_counter = {}
 
@@ -7,11 +7,11 @@ while True:
         number = int(input("Enter numbers: "))
     except ValueError:
         break
-
     if number in number_counter:
         number_counter[number] += 1
     else:
         number_counter[number] = 1
-
-print(number_counter)
+if number_counter:
+    most_frequent = max(number_counter, key=number_counter.get)
+    print("The number with the most duplicates is:", most_frequent)
 
