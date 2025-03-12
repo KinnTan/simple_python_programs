cf755d549fd2757ead19d555e64ea53d43f2eeab
Kinn Tan <tankinnr@gmail.com>
2025-03-12 21:49:02 +0800

    add exception handler to break loop properly on invalid input

--- Batch 4/most_frequent_number.py
+++ Batch 4/most_frequent_number.py
@@ -1,6 +1,9 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most numbe
 
-while True:
-        number = int(input("Enter numbers: "))
 
+while True:
+    try:
+        number = int(input("Enter numbers: "))
+    except ValueError:
+        break
 
