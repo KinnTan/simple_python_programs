8278633d022c8848b26c4cc83dd6a4782909bedf
Kinn Tan <tankinnr@gmail.com>
2025-03-12 21:22:01 +0800

    Implemented logic to find and store duplicate numbers from user input

--- Batch 4/print_duplicate_numbers.py
+++ Batch 4/print_duplicate_numbers.py
@@ -1,4 +1,15 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+duplicate = []
+number_list = []
+
 for i in range(1,11):
     numbers = int(input())
+    if numbers in number_list:
+        duplicate.append(numbers)
+        number_list.append(numbers)
+    else:
+        number_list.append(numbers)
+
+print(duplicate)
+
