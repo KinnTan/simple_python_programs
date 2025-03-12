0d420583a310f0681c78388aba09903acac009ae
Kinn Tan <tankinnr@gmail.com>
2025-03-12 21:59:16 +0800

    Initialize number_counter Track and count occurrences of entered numbers

--- Batch 4/most_frequent_number.py
+++ Batch 4/most_frequent_number.py
@@ -1,9 +1,17 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most numbe
 
+number_counter = {}
 
 while True:
     try:
         number = int(input("Enter numbers: "))
     except ValueError:
         break
+
+    if number in number_counter:
+        number_counter[number] += 1
+    else:
+        number_counter[number] = 1
+
+print(number_counter)
 
