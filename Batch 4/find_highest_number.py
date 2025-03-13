864f108f247d9b3cd8e0339342e7ebf28cf71dc6
Kinn Tan <tankinnr@gmail.com>
2025-03-13 10:26:39 +0800

    - initialize number_list - append number to number_list - print highest number on number_list using max()

--- Batch 4/find_highest_number.py
+++ Batch 4/find_highest_number.py
@@ -1,7 +1,12 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 
+number_list = []
+
 while True:
     try:
         number = int(input("Enter numbers: "))
     except ValueError:
         break
+    number_list.append(number)
+
+print(max(number_list))
