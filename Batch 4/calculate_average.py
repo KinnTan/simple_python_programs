863898d13d0b9b69a54378d0f03998f9a611f2fc
Kinn Tan <tankinnr@gmail.com>
2025-03-13 10:57:40 +0800

    - initialize number_list - append number to number_list - get average by dividing sum() to len() of number_list - add label on print for clarity

--- Batch 4/calculate_average.py
+++ Batch 4/calculate_average.py
@@ -1,7 +1,12 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+numbers = []
 while True:
     try:
-        number = int(input("Enter a number: "))
+        input_number = int(input("Enter a number: "))
+        numbers.append(input_number)
+
     except ValueError:
+        average = sum(numbers)/len(numbers)
+        print("The average of the entered numbers is:", average)
         break
